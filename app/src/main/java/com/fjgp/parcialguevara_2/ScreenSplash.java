4f2d568e41f2275e8a9945928de49191ca02e84e
FavioJesus <faviogpfk@gmail.com>
2022-11-17 14:03:53 -0500

    favio

--- app/src/main/java/com/fjgp/parcialguevara_2/ScreenSplash.java
+++ app/src/main/java/com/fjgp/parcialguevara_2/ScreenSplash.java
@@ -1,8 +1,12 @@
 package com.fjgp.parcialguevara_2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ScreenSplash extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class ScreenSplash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        TimerTask tarea= new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(ScreenSplash.this,inicio.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,2000);
     }
 }
--- app/src/main/java/com/fjgp/parcialguevara_2/inicio.java
+++ app/src/main/java/com/fjgp/parcialguevara_2/inicio.java
@@ -2,13 +2,41 @@ package com.fjgp.parcialguevara_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class inicio extends AppCompatActivity {
-
+public class inicio extends AppCompatActivity implements View.OnClickListener {
+    Button btnIngresar,btnRegistrarseInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        btnIngresar = (Button) findViewById(R.id.button);
+        btnRegistrarseInicio = (Button) findViewById(R.id.button3);
+        btnIngresar.setOnClickListener(this);
+        btnRegistrarseInicio.setOnClickListener(this);
     }
+
+    public void IngresarAlApp() {
+        Intent intent = new Intent(inicio.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void RegistrarseEnElApp() {
+        Intent intent = new Intent(inicio.this, RegistrarActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnIngresar) {
+            IngresarAlApp();
+        } else if (v == btnRegistrarseInicio) {
+            RegistrarseEnElApp();
+        }
+    }
+
 }
